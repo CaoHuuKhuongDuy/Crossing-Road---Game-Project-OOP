9771fa1a35f931346b791dc7472295aeec6fa2fb
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2023-10-20 02:42:05 +0700

    Update screen.h

--- src/screen.h
+++ src/screen.h
@@ -2,21 +2,26 @@
 
 #include "staticVariable.h"
 #include"entity.h"
+#include "button.h"
 #include <iostream>
 
 using namespace stValue;
 using namespace std;
 
-
-void GotoXY(int x, int y);
-
-
 class Screen {
     public:
         Screen(int width = FIX_WIDTH, int height = FIX_HEIGHT);
-        void draw();
-
+        virtual void draw() = 0;
     private:
         int width, height;
 };
 
+class menuScreen: public Screen {
+    public:	
+        menuScreen();
+    	void draw() override;
+    private:
+    	string pathColorCode;
+    	string Store[12] = {"crossingroad","newgame","loadgame","leaderboard","setting","credit","exit","frame","star","rocket","sartun","jupiter"};
+    	
+};
