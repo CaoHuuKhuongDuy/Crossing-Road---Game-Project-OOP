82531767e498cdb65ad88fef5c27599df16bd922
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2023-10-21 00:26:55 +0700

    CrossingRoad.cpp

--- src/CrossingRoad.cpp
+++ src/CrossingRoad.cpp
@@ -8,17 +8,15 @@ signed main() {
     stValue::appConsole.init();
     stValue::importImage.init(&stValue::appConsole);
     char keyPress = 0;
+	appConsole.setBackgroundColor(BG_BLUE);
+	int debug = 0;
     appConsole.setFullscreenBackgroundColor(BG_BLUE);
-    DynamicEntity *rocket = new DynamicEntity("rocket.txt", {100, 35}, {8, 15}, string(1, char(219)), false);
-
-    int debug = 0;
+    menuScreen* menu = new menuScreen();
+    menu->draw();
     while (true) {
-	    rocket->draw();
-        debug++;
-        rocket->right(1);
-        Sleep(10);
-        // return 0;
+    	
     }
+    delete menu;
    int x;
    cin >> x;
    return 0;
