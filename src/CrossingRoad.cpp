326e6003265f4cdc2f36c5eb2025a27544f8476d
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2023-10-21 01:47:01 +0700

    Update CrossingRoad.cpp

--- src/CrossingRoad.cpp
+++ src/CrossingRoad.cpp
@@ -8,8 +8,7 @@ signed main() {
     stValue::appConsole.init();
     stValue::importImage.init(&stValue::appConsole);
     char keyPress = 0;
-	appConsole.setBackgroundColor(BG_BLUE);
-	int debug = 0;
+    int debug = 0;
     appConsole.setFullscreenBackgroundColor(BG_BLUE);
     menuScreen* menu = new menuScreen();
     menu->draw();
