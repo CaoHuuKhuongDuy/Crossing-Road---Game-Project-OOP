473c8d1ab484435c8310099b091817b3da08deda
chitien2808 <chitien2808@gmail.com>
2023-10-21 13:39:55 +0700

    fix size image

--- src/screen.cpp
+++ src/screen.cpp
@@ -3,23 +3,22 @@
 
 Screen::Screen(int width_, int height_) : width(width_), height(height_)  {} 
 
-menuScreen:: menuScreen(): pathColorCode("../media/asciiCode_"){};
-
+menuScreen:: menuScreen() {};
 
 void menuScreen:: draw() {
-	Button* star =  new Button(pathColorCode+Store[8],{0,0},WHITE,GREEN);
+	Button* star =  new Button(Store[8],{0,0},WHITE,GREEN);
 	star->draw();
-	Button* button = new Button(pathColorCode+Store[0],{36,5},WHITE,GREEN);
+	Button* button = new Button(Store[0],{36,5},WHITE,GREEN);
 	button->draw(); 
-	Button* frame = new Button(pathColorCode+Store[7],{45,12},WHITE,GREEN);
+	Button* frame = new Button(Store[7],{45,12},WHITE,GREEN);
 	frame->draw();
 	int choices = 0;
 	Button* choose;
 	Button* clearChoose;
 	DynamicEntity *rocket = new DynamicEntity("rocket.txt", {1,10}, {8, 18}, string(1, char(219)), false);
-	Entity *jupiter = new Entity("Jupiter.txt", {20,22}, {0,0});
-	Entity *sartun = new Entity("Sartun.txt", {170,7}, {0,0});
-	Entity *venus = new Entity("Venus.txt", {140,31}, {0,0});
+	Entity *jupiter = new Entity("jupiter.txt", {20,22}, {12,11});
+	Entity *sartun = new Entity("sartun.txt", {170,7}, {21, 20});
+	Entity *venus = new Entity("venus.txt", {140,31}, {23, 15});
 	int idx ;
 	int move = 8;
 
@@ -30,9 +29,9 @@ void menuScreen:: draw() {
 		{
 			if (choices != i)
 			{
-				clearChoose = new Button(pathColorCode + Store[10], {50, SHORT(i * 4 + 13)}, WHITE, GREEN);
+				clearChoose = new Button(Store[10], {50, SHORT(i * 4 + 13)}, WHITE, GREEN);
 				clearChoose->draw();
-				buttons[i] = new Button(pathColorCode + Store[i + 1], {60, SHORT(i * 4 + 14)}, WHITE, GREEN);
+				buttons[i] = new Button(Store[i + 1], {60, SHORT(i * 4 + 14)}, WHITE, GREEN);
 				buttons[i]->draw();
 			}
 			else
@@ -41,9 +40,9 @@ void menuScreen:: draw() {
 			}
 		}
 		if(choices==idx){
-			choose = new Button(pathColorCode+Store[9],{50,SHORT(idx*4+13)},WHITE,GREEN);
+			choose = new Button(Store[9],{50,SHORT(idx*4+13)},WHITE,GREEN);
 			choose->draw();
-			buttons[idx] =  new Button(pathColorCode+Store[idx+1],{60,SHORT(idx*4+14)},WHITE,GREEN);
+			buttons[idx] =  new Button(Store[idx+1],{60,SHORT(idx*4+14)},WHITE,GREEN);
 			buttons[idx]->draw();
 		}
 
