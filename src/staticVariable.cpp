7c76572a7efe25e051411186e086122082bcf955
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2023-11-30 23:11:53 +0700

    Update staticVariable.cpp

--- src/staticVariable.cpp
+++ src/staticVariable.cpp
@@ -3,7 +3,7 @@
 
 namespace stValue {
 	COORD FIX_SIZE = {211, 54};
-    COORD FONT_SIZE = {7, 16};
+    COORD FONT_SIZE = {12, 16};
     int DEFAULT_STEP = 5;
 	string GAME_NAME = "Crossing Road";
 	Console appConsole;
@@ -13,6 +13,7 @@ namespace stValue {
     Player *mainPlayer = nullptr;
     Sound sound;
     bool typeHero = 0;
+    int idPlayer = 5; 
     void init() {
         appConsole.init();
         importImage.init(&appConsole);
