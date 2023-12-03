c7fcc5af7d8a6b27c21bcb8c38bba64f41aef264
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2023-12-03 13:49:36 +0700

    Update handlerInput.cpp

--- src/handlerInput.cpp
+++ src/handlerInput.cpp
@@ -169,7 +169,7 @@ Command *HandlerLoadInput::handlerInput(ButtonList &buttonList) {
     }
     if (userInput == 13) {
         loadSavedGame->setIdButtonChoosen(buttonList.getIdButtonChoosen());
-	idPlayer = buttonList.getIdButtonChoosen();    
+        idPlayer = buttonList.getIdButtonChoosen();
         return loadSavedGame;
     }
     return nullptr;
