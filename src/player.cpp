8d8b21ed915976d69fb16bbb68a88b48e48e2b8e
Banhmikepthit0105 <113972405+Banhmikepthit0105@users.noreply.github.com>
2023-12-07 14:16:29 +0700

    Updated

--- src/player.cpp
+++ src/player.cpp
@@ -34,6 +34,9 @@ void ListPlayer::init() {
     string name, level, score;
     while (file >> name >> level >> score) {
         Player p(name, level, score);
+        int levelInt = stoi(level);
+        int scoreInt = (levelInt-1) * 300;
+        p.setScore(to_string(scoreInt));
         players.push_back(p);
     }
     file.close();
